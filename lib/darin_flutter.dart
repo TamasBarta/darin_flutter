765a2d691495b0399b8b1d80750b3e37976a6fe7
Tamás Barta <barta.tamas.d@gmail.com>
2021-12-01 03:08:54 +0100

    First commit

---
+++ lib/darin_flutter.dart
@@ -0,0 +1,52 @@
+library darin_flutter;
+
+import 'package:darin/darin.dart';
+import 'package:flutter/material.dart';
+
+class Darin extends InheritedWidget {
+  const Darin({
+    Key? key,
+    required this.module,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final Module module;
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+
+  static Module scopeOf(BuildContext context) {
+    final darinWidget = context.dependOnInheritedWidgetOfExactType<Darin>();
+    if (darinWidget == null) {
+      throw Exception(
+          "The Darin widget cannot be found in the current context.");
+    }
+    return darinWidget.module;
+  }
+
+  static T get<T>(BuildContext context, {dynamic qualifier}) =>
+      scopeOf(context).get<T>(qualifier: qualifier);
+
+  static T getMap<T extends Map>(BuildContext context, {dynamic qualifier}) =>
+      scopeOf(context).getMap<T>(qualifier: qualifier);
+
+  static T getSet<T extends Set>(BuildContext context, {dynamic qualifier}) =>
+      scopeOf(context).getSet<T>(qualifier: qualifier);
+
+  static Module newScopeOf<T>(BuildContext context, T target) =>
+      scopeOf(context).scope(target);
+}
+
+extension DarinContext on BuildContext {
+  Module darinScope() => Darin.scopeOf(this);
+
+  T darinGet<T>({dynamic qualifier}) => Darin.get(this, qualifier: qualifier);
+
+  T darinGetMap<T extends Map>({dynamic qualifier}) =>
+      Darin.getMap(this, qualifier: qualifier);
+
+  T darinGetSet<T extends Set>({dynamic qualifier}) =>
+      Darin.getSet(this, qualifier: qualifier);
+
+  Module newDarinScope<T>(T target) => Darin.newScopeOf(this, target);
+}
---
+++ test/darin_flutter_test.dart
@@ -0,0 +1,25 @@
+import 'package:darin/darin.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:darin_flutter/darin_flutter.dart';
+
+void main() {
+  testWidgets('Basic inheritance', (WidgetTester tester) async {
+    final module = Module(
+      (module) => module.factory((module) => "Injected text"),
+    );
+
+    await tester.pumpWidget(
+      Darin(
+        module: module,
+        child: Builder(
+          builder: (context) => MaterialApp(home: Text(context.darinGet())),
+        ),
+      ),
+    );
+
+    final text = find.text("Injected text");
+    expect(text, findsOneWidget);
+  });
+}
